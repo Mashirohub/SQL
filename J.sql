b9e39cff0e17bf254022230c3a377cb1b3f11046
Tanawat Wongsanoa <mashiroff23@gmail.com>
2025-07-17 15:45:24 +0700

    Sql

---
+++ J.sql
@@ -0,0 +1,9 @@
+select EmployeeID, FirstName, LastName 
+from Employees
+where City = 'London'
+
+--เเสดงชื่อเมือง เเละประเทส
+select City, Country from Customers
+
+
+select distinct city, country from customers
