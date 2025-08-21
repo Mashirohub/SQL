2a37f91c345dc10bc867c06af802b3e8a0f34a66
Tanawat Wongsanoa <mashiroff23@gmail.com>
2025-08-21 13:54:51 +0700

    Update SQL_Work.sql

--- SQL_Work.sql
+++ SQL_Work.sql
@@ -1,3 +1,39 @@
+--1
+select c.CategoryName, p.ProductName, p.UnitPrice
+from Products as p, Categories as c
+where p.CategoryID=c.CategoryID
+--2
 select CategoryName, ProductName, UnitPrice
-from Products, Categories
-where Products.CategoryID=Categories.CategoryID
+from Products Join Categories
+on Products.CategoryID=Categories.CategoryID
+
+select c.CategoryName, p.ProductName, p.UnitPrice
+from Products as p, Categories as c
+where p.CategoryID=c.CategoryID
+and CategoryName = 'seafood'
+
+select c.CategoryName,p. ProductName, p.UnitPrice
+from p.Products Join c.Categories
+on p.Products=c.CategoryID
+
+select CompanyName, OrderID
+from Orders, Shippers
+where Shippers.ShipperID = Orders.ShipVia
+
+select CompanyName, OrderID
+from Orders join Shippers
+on Shippers.ShipperID = Orders.ShipVia
+
+select CompanyName, OrderID
+from Orders,Shippers
+where Shippers.ShipperID = Orders.ShipVia
+
+select CompanyName, OrderID
+from Orders,Shippers
+where Shippers.ShipperID = Orders.ShipVia
+and OrderID = 10275
+
+select CompanyName, OrderID
+from Orders join Shippers
+on Shippers.ShipperID = Orders.ShipVia
+where OrderID = 10275
